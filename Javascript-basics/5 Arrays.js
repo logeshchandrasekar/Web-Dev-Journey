1586b96c3806d2d75677b88630ecb8759f001b63
Logeshwar Chandrasekar <114049434+logeshchandrasekar@users.noreply.github.com>
2026-05-14 23:41:30 +0530

    Add array creation and manipulation examples

---
+++ Javascript-basics/5 Arrays.js
@@ -0,0 +1,31 @@
+// Creating array
+const hobbies =  ['learn','code','post'];
+console.log(hobbies)
+
+//access elements
+const numbers = ['One', 'Two', 'Three'];
+const listItem = (famousSayings[0]);
+console.log(listItem);
+console.log(numbers[2]);
+console.log(numbers[3]);
+
+// Updating elements in array
+let groceryList = ['bread', 'tomatoes', 'milk'];
+groceryList[1] = 'avocados';
+
+// Arrays with let and const
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+
+condiments = ['Mayo'];
+console.log(condiments);
+
+utensils[3] = 'Spoon';
+console.log(utensils);
+
+utensils = ['Spoon'];
+console.log(utensils);
