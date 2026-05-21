1d236ba38fb843028915014513700a5cc864b4d5
Logeshwar Chandrasekar <114049434+logeshchandrasekar@users.noreply.github.com>
2026-05-21 22:39:04 +0530

    Add comments explaining array iterator methods

--- Frontend/Javascript-basics/8 Iterators.js
+++ Frontend/Javascript-basics/8 Iterators.js
@@ -44,3 +44,13 @@ const newSum = newNumbers.reduce((accumulator,currentValue) => {
   return accumulator + currentValue;
 },10)
 console.log('Final value: ' + newSum);
+
+/*
+- .forEach() is used to execute the same code on every element in an array, but does not change the array and returns undefined.
+- .map() executes the same code on every element in an array and returns a new array with the updated elements.
+- .filter() checks every element in an array to see if it meets certain criteria and returns a new array with the elements that return truthy for the criteria.
+- .findIndex() returns the index of the first element of an array that satisfies a condition in the callback function. It returns -1 if none of the elements in the array satisfy the condition.
+- .reduce() iterates through an array and takes the values of the elements and returns a single value.
+- All iterator methods take a callback function, which can be a pre-defined function, a function expression, or an arrow function.
+- visit the Mozilla Developer Network to learn more about iterator methods.
+*/
