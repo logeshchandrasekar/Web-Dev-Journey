d7c8084e88f5d86b2012f7b1ac6127d1f3aaaede
Logeshwar Chandrasekar <114049434+logeshchandrasekar@users.noreply.github.com>
2026-05-21 21:00:02 +0530

    Add examples of findIndex method in iterators

--- Frontend/Javascript-basics/8 Iterators.js
+++ Frontend/Javascript-basics/8 Iterators.js
@@ -34,3 +34,10 @@ console.log(longFavoriteWords);
 console.log(shortWords);
 
 // The .findIndex() method
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+const foundAnimal = animals.findIndex(animal => {
+  return animal === 'elephant';
+});
+const startsWithS = animals.findIndex(animal => {
+  return animal[0] === 's';
+})
