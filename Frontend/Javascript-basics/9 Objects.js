8ac2f68598c77a0cf94c9cf6069eec063e7a911b
Logeshwar Chandrasekar <114049434+logeshchandrasekar@users.noreply.github.com>
2026-05-22 17:39:16 +0530

    Update 9 Objects.js

--- Frontend/Javascript-basics/9 Objects.js
+++ Frontend/Javascript-basics/9 Objects.js
@@ -153,3 +153,15 @@ for (let crewMember in spaceship.crew) {
 for (let crewMember in spaceship.crew) {
   console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
 };
+
+/*
+- Objects store collections of key-value pairs.
+- Each key-value pair is a property — when a property is a function, it is known as a method.
+- An object literal is composed of comma-separated key-value pairs surrounded by curly braces.
+- We can access, add, or edit a property within an object by using dot notation or bracket notation.
+- We can add methods to our object literals using key-value syntax with anonymous function expressions as values or by using the new ES6 method syntax.
+- We can navigate complex, nested objects by chaining operators.
+- Objects are mutable — we can change their properties even when they’re declared with const.
+- Objects are passed by reference — when we make changes to an object passed into a function, those changes are permanent.
+- We can iterate through objects using the for...in syntax.
+*/
