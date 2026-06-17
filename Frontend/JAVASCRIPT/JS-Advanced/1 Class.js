0bd7dfa4d0b0e49687dcd939e4449db8599dac42
Logeshwar Chandrasekar <114049434+logeshchandrasekar@users.noreply.github.com>
2026-06-17 23:29:39 +0530

    Update 1 Class.js

--- Frontend/JAVASCRIPT/JS-Advanced/1 Class.js
+++ Frontend/JAVASCRIPT/JS-Advanced/1 Class.js
@@ -1,34 +1,47 @@
-//General Structure of a class with getters and methods.
-class Surgeon {
-  constructor(name, department) {
-    this._name = name;
-    this._department = department;
+// General Class Structure is written
+
+// Superclass :
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name; // use underscore after this. keyword to make the property private, always use ._
     this._remainingVacationDays = 20;
   }
-
-  // getters in class
+  // getter
   get name() {
     return this._name;
   }
-  
-  get department() {
-    return this._department;
-  }
-  
   get remainingVacationDays() {
     return this._remainingVacationDays;
   }
-
-  // method in class
+  // method
   takeVacationDays(daysOff) {
     this._remainingVacationDays -= daysOff;
   }
 }
+// Subclass :
+class Nurse extends HospitalEmployee { // here 'extends' makes the Nurse class inherits the Superclass's properties
+  constructor(name, certifications) {
+    super(name); // super refers to the constructor of the superclass which gets called in subclass
+    this._certifications = certifications;
+  }
+  // getter
+  get certifications() {
+    return this._certifications;
+  }
+  // method
+  addCertification(newCertification){
+    this._certifications.push(newCertification);
+  }
+}
 
-const surgeonLogesh = new Surgeon('Logeshwar', 'Cardiovascular'); // new instance
-const surgeonChandru = new Surgeon('Chandrasekar', 'Orthopedics'); // new instance
+const nursePriya = new Nurse('Priya', ['Trauma','Pediatrics']); // An instance created using subclass
+nursePriya.takeVacationDays(5); // calling a method in super class, then inputs '5'
+console.log(nursePriya.remainingVacationDays); // logs value by using a getter from superclass
+nursePriya.addCertification('Genetics'); // adding a array input in the instance using a method from subclass
+console.log(nursePriya.certifications); // logs the values from the instance using a getter from subclass
 
-console.log(surgeonLogesh.name); //logs name
-console.log(surgeonLogesh.department); //logs department
-surgeonLogesh.takeVacationDays(3); // updates input by calling method with a argument
-console.log(surgeonlogesh.remainingVacationDays); // logs new value
+// we can create as many subclasses as we want including getters and methods in it.
+/* Reason for using a superclass is
+- better readability 
+- reducing number of lines
+- to make convenient changes on common properties shared by many subclasses. 
