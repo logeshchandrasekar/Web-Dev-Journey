d75777b4f49302c6cff6b0b7c358d1857f4850e4
Logeshwar Chandrasekar <114049434+logeshchandrasekar@users.noreply.github.com>
2026-06-29 14:15:54 +0530

    Refactor serveDinnerAgain to use Promise.all
    Updated serveDinnerAgain function to use Promise.all for concurrent execution of cooking functions.

--- Frontend/JAVASCRIPT/JS-Advanced/4 Async & Await.js
+++ Frontend/JAVASCRIPT/JS-Advanced/4 Async & Await.js
@@ -219,7 +219,24 @@ serveDinner();
 
 //-------------------------------------------------------------------------------------------------------------------------------------//
 
-// 6. Handling errors using try & catch :
+// 6. Using await promise.all() :
+	// import the cooking functions from the same file
+let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./4 Async & Await.js');
+
+async function serveDinnerAgain() {
+	// Run all promises in parallel and wait for all to resolve
+  let foodArray = await Promise.all([steamBroccoli(), cookRice(), bakeChicken(), cookBeans()]);
+
+	// Log the results using array indices
+  console.log(`Dinner is served. We're having ${foodArray[0]}, ${foodArray[1]}, ${foodArray[2]}, and ${foodArray[3]}.`);
+}
+
+	// Invoke the async function
+serveDinnerAgain();
+
+//-------------------------------------------------------------------------------------------------------------------------------------//
+
+// 7. Handling errors using try & catch :
 let randomNum = () => {
  let num = Math.random();
  if (num < .5 ){
@@ -262,3 +279,14 @@ async function hostDinnerParty() {
 hostDinnerParty();
 
 //-------------------------------------------------------------------------------------------------------------------------------------//
+
+/* General Notes :
+- async...await is syntactic sugar built on native JavaScript promises and generators.
+- We declare an async function with the keyword async.
+- Inside an async function, we use the await operator to pause execution of our function until an asynchronous action completes and the awaited 
+  promise is no longer pending.
+- await returns the resolved value of the awaited promise.
+- We can write multiple await statements to produce code that reads like synchronous code.
+- We should still take advantage of concurrency by writing async functions that allow asynchronous actions to happen concurrently whenever possible.
+- We use try...catch statements within our async functions for error handling.
+*/
