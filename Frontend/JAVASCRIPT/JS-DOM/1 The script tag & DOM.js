b85a4872fa105b71e6424a9c4c21a2d2e77ae8cc
Logeshwar Chandrasekar <114049434+logeshchandrasekar@users.noreply.github.com>
2026-07-03 21:04:26 +0530

    Update 1 The script tag & DOM.js

--- Frontend/JAVASCRIPT/JS-DOM/1 The script tag & DOM.js
+++ Frontend/JAVASCRIPT/JS-DOM/1 The script tag & DOM.js
@@ -2,19 +2,19 @@
 <script>
   // The js logic code we write here with 'DOM' + html elements goes here.
   // It makes the static web page into a dynamic web page with interactivity.
-  </script>
+</script>
 
 <script src = "./script.js">
   // writes the js logic code in a seperate file and links to the html file.
-  </script>
+</script>
 
 // 2. What is DOM ?
-// - The DOM (Document Object Model) is the bridge between your static HTML and your dynamic JavaScript.
-/* - When a browser loads an HTML file, it doesn't just "display text.",
+  // - The DOM (Document Object Model) is the bridge between your static HTML and your dynamic JavaScript.
+  /* - When a browser loads an HTML file, it doesn't just "display text.",
      It parses that HTML and builds a live, in-memory 'tree structure' out of it called the DOM.
   
-The Tree Structure :
-  - Given this HTML: */
+  The Tree Structure :
+    - Given this HTML: 
     <html>
       <body>
         <h1>Hello</h1>
@@ -22,14 +22,14 @@ The Tree Structure :
       </body>
     </html>
 
-/* - The browser builds a tree like this:
+   - The browser builds a tree like this:
 document
  └── html
       └── body
            ├── h1 ("Hello")
-           └── p ("Welcome to my site") */
+           └── p ("Welcome to my site")
            
- /* Here, Every tag becomes a node.
+  Here, Every tag becomes a node.
     document is the root of everything — it's your entry point into this tree from JavaScript.*/
 
 /* 3. Example : TO TRY THIS ON YOUR OWN AND GRASP THE CONCEPT :
