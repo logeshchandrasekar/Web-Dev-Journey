8f1fc4481a1a0077a2765a91795dc108258cc4d1
Logeshwar Chandrasekar <114049434+logeshchandrasekar@users.noreply.github.com>
2026-09-07 22:40:23 +0530

    Enhance SQL queries with ORDER BY examples
    Added examples for using ORDER BY clause in SQL queries.

--- Database/SQL/3. Queries.sql
+++ Database/SQL/3. Queries.sql
@@ -115,3 +115,21 @@ SELECT *
 FROM movies
 WHERE year > 2014
    OR genre = 'action';
+
+-- 10.ORDER BY :
+-- We can sort the results using ORDER BY, either alphabetically or numerically. Sorting the results often makes the data more useful and easier to analyze.
+  --EX: if we want to sort everything by the movie’s title from A through Z:
+SELECT *
+FROM movies
+ORDER BY name;
+--ORDER BY is a clause that indicates you want to sort the result set by a particular column.
+-- Sometimes we want to sort things in a decreasing order.
+  --EX: if we want to select all of the well-received movies, sorted from highest to lowest by their year:
+SELECT *
+FROM movies
+WHERE imdb_rating > 8
+ORDER BY year DESC;
+-- DESC is a keyword used in ORDER BY to sort the results in descending order (high to low or Z-A).
+-- ASC is a keyword used in ORDER BY to sort the results in ascending order (low to high or A-Z).
+-- The column that we ORDER BY doesn’t even have to be one of the columns that we’re displaying.
+-- Note: ORDER BY always goes after WHERE (if WHERE is present).
