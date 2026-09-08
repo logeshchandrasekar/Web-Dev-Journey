9237cfdac36bea755cbed5281211be77113ef218
Logeshwar Chandrasekar <114049434+logeshchandrasekar@users.noreply.github.com>
2026-09-08 11:16:39 +0530

    Enhance SQL file with summary of query clauses
    Added summary comments explaining SQL query clauses.

--- Database/SQL/3. Queries.sql
+++ Database/SQL/3. Queries.sql
@@ -169,3 +169,15 @@ SELECT name,
  END AS 'Review'
 FROM movies;
 -- Now the new column's name won't be that long. Also dont forget to add comma before the CASE Statement.
+
+
+-- SUMMARY :
+  -- SELECT is the clause we use every time we want to query information from a database.
+  -- AS renames a column or table.
+  -- DISTINCT return unique values.
+  -- WHERE is a popular command that lets you filter the results of the query based on conditions that you specify.
+  -- LIKE and BETWEEN are special operators.
+  -- AND and OR combines multiple conditions.
+  -- ORDER BY sorts the result.
+  -- LIMIT specifies the maximum number of rows that the query will return.
+  -- CASE creates different outputs.
