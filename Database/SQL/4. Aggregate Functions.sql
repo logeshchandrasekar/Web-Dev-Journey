82aeee76490da5c1a50999b0a1f8c9a043ae7ddf
Logeshwar Chandrasekar <114049434+logeshchandrasekar@users.noreply.github.com>
2026-09-09 09:47:44 +0530

    Enhance documentation on SQL aggregate functions
    Added explanations for HAVING clause and aggregate functions.

--- Database/SQL/4. Aggregate Functions.sql
+++ Database/SQL/4. Aggregate Functions.sql
@@ -44,9 +44,10 @@ FROM apps;
 -- SQL rounds the values in the column to 0 decimal places in the output.
 
 -- 6.GROUP BY :
+-- Groups a result set based on an aggregate function (COUNT(), MIN(), MAX(), SUM(), AVG()). It lists the number in each group.
 -- Oftentimes, we will want to calculate an aggregate for data with certain characteristics.
--- For instance, we might want to know the mean IMDb ratings for all movies each year.
--- We could calculate each number by a series of queries with different WHERE statements, like so:
+  -- EX: For instance, we might want to know the mean IMDb ratings for all movies each year.
+  -- We could calculate each number by a series of queries with different WHERE statements, like so:
 SELECT AVG(imdb_rating)
 FROM movies
 WHERE year = 1999;
@@ -58,16 +59,64 @@ WHERE year = 2000;
 SELECT AVG(imdb_rating)
 FROM movies
 WHERE year = 2001;
-
--- and so on.
--- Luckily, there’s a better way!
--- We can use GROUP BY to do this in a single step:
+  -- and so on. But luckily, there’s a better way!
+  -- We can use GROUP BY to do this in a single step:
 SELECT year,
    AVG(imdb_rating)
 FROM movies
 GROUP BY year
 ORDER BY year;
+  -- GROUP BY is a clause in SQL that is used with aggregate functions.
+  -- It is used in collaboration with the SELECT statement to arrange identical data into groups.
+  -- The GROUP BY statement comes after any WHERE statements, but before ORDER BY or LIMIT.
 
--- GROUP BY is a clause in SQL that is used with aggregate functions.
--- It is used in collaboration with the SELECT statement to arrange identical data into groups.
--- The GROUP BY statement comes after any WHERE statements, but before ORDER BY or LIMIT.
+-- Sometimes, we want to GROUP BY a calculation done on a column.
+  -- EX: For instance, we might want to know how many movies have IMDb ratings that round to 1, 2, 3, 4, 5. We could do this using the following syntax:
+SELECT ROUND(imdb_rating),
+   COUNT(name)
+FROM movies
+GROUP BY ROUND(imdb_rating)
+ORDER BY ROUND(imdb_rating);
+  -- However, this query may be time-consuming to write and more prone to error.
+
+  -- SQL lets us use column reference(s) in our GROUP BY that will make our lives easier.
+    -- 1 is the first column selected
+    -- 2 is the second column selected
+    -- 3 is the third column selected
+  -- and so on.
+
+-- The following query is equivalent to the one above:
+SELECT ROUND(imdb_rating),
+   COUNT(name)
+FROM movies
+GROUP BY 1
+ORDER BY 1;
+-- Here, the 1 refers to the first column in our SELECT statement, ROUND(imdb_rating).
+
+-- 7.HAVING() :
+-- In addition to being able to group data using GROUP BY, SQL also allows you to filter which groups to include and which to exclude.
+  -- EX: imagine that we want to see how many movies of different genres were produced each year, but we only care about years and genres with at least 10 movies.
+  -- We can’t use WHERE here because we don’t want to filter the rows; we want to filter groups.
+  -- This is where HAVING comes in.
+-- HAVING is very similar to WHERE. In fact, all types of WHERE clauses can be used with HAVING.
+-- We can use the following for the problem:
+SELECT year,
+   genre,
+   COUNT(name)
+FROM movies
+GROUP BY 1, 2
+HAVING COUNT(name) > 10;
+-- When we want to limit the results of a query based on values of the individual rows, use WHERE.
+-- When we want to limit the results of a query based on an aggregate property, use HAVING.
+-- HAVING statement always comes after GROUP BY, but before ORDER BY and LIMIT.
+
+
+-- SUMMARY :
+  -- COUNT() : count the number of rows
+  -- SUM() : the sum of the values in a column
+  -- MAX() / MIN() : the largest/smallest value
+  -- AVG() : the average of the values in a column
+  -- ROUND() : round the values in the column
+-- Aggregate functions combine multiple rows together to form a single value of more meaningful information.
+  -- GROUP BY is a clause used with aggregate functions to combine data from one or more columns.
+  -- HAVING limit the results of a query based on an aggregate property.
